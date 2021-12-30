06134367e72b62834699abdeb6be8e2115da3fb8
preciousimo <preciousimoniakemu@gmail.com>
2021-12-31 00:46:20 +0100

    Templates

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('room/', views.room, name="room"),
+    path('room/<str:pk>/', views.room, name="room"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+rooms = [
+    {'id': 1, 'name': 'Lets Learn Python!'},
+    {'id': 2, 'name': 'Design With Me'},
+    {'id': 3, 'name': 'Frontend Developers'},
+]
+
 
 def home(request):
-    return HttpResponse('Home page')
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return HttpResponse('ROOM')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
