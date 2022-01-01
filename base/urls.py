df8a1a4af89648ccfbc79c890c87472764c81b04
preciousimo <preciousimoniakemu@gmail.com>
2022-01-01 17:18:19 +0100

    User Login

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name="login"),
+    
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
     
--- base/views.py
+++ base/views.py
@@ -1,8 +1,21 @@
+import email
 from django.shortcuts import render, redirect
 from django.db.models import Q
+from django.contrib.auth.models import User
 from .models import Room, Topic
 from .forms import RoomForm
 
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            pass
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
