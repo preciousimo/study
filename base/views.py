a9189d0baa91e87c87f123bfa30f3b5a8d9a259e
preciousimo <preciousimoniakemu@gmail.com>
2022-01-01 11:31:55 +0100

    Search Functionality

--- base/views.py
+++ base/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) |
+                                Q(name__icontains=q) |
+                                Q(description__icontains=q)
+                                )
+    
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
