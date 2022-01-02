7061e0e09c60f4cf5f7e16df25505697f2460e33
preciousimo <preciousimoniakemu@gmail.com>
2022-01-02 23:42:18 +0100

    Flash messages

--- base/views.py
+++ base/views.py
@@ -1,7 +1,9 @@
 import email
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -13,7 +15,16 @@ def loginPage(request):
         try:
             user = User.objects.get(username=username)
         except:
-            pass
+            messages.error(request, 'User does not exist')
+            
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password is required')
+            
     context = {}
     return render(request, 'base/login_register.html', context)
 
