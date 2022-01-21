c329a3aa78314caa004c11912b4c0c8aa9274cd3
preciousimo <preciousimoniakemu@gmail.com>
2022-01-21 13:31:35 +0100

    User Logout

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
     
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.messages import get_messages
 from .models import Room, Topic
 from .forms import RoomForm
@@ -28,6 +28,10 @@ def loginPage(request):
             
     context = {}
     return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
