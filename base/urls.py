dc8c8fc42fddb5c2fbf697c434b158c8017a90a8
preciousimo <preciousimoniakemu@gmail.com>
2022-01-22 17:48:58 +0100

    User registration

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
     
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
--- base/views.py
+++ base/views.py
@@ -6,16 +6,19 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
 def loginPage(request):
     
+    page = 'login'
+    
     if request.user.is_authenticated:
         return redirect('home')
     
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         
         try:
@@ -31,12 +34,28 @@ def loginPage(request):
         else:
             messages.error(request, 'Username OR password does not exist')
             
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+ 
+def registerPage(request):
+    form = UserCreationForm()
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')            
+            
+    return render(request, 'base/login_register.html', {'form':form})
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
