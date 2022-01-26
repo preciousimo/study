465a75d2373627e9eb5b13c35d811920f0dd6afe
preciousimo <preciousimoniakemu@gmail.com>
2022-01-26 07:51:55 +0100

    Activity feed

--- base/models.py
+++ base/models.py
@@ -31,5 +31,8 @@ class Message(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
+    class Meta:
+        ordering = ['-updated', '-created']
+    
     def __str__(self):
         return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -67,13 +67,14 @@ def home(request):
     
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
     
-    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count, 'room_messages':room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
     if request.method == 'POST':
         message = Message.objects.create(
