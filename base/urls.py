9ffae9f0d367fd5bc66ea9cbee1711a426b249a1
preciousimo <preciousimoniakemu@gmail.com>
2022-01-29 17:57:07 +0100

    Mobile Responsiveness

--- base/urls.py
+++ base/urls.py
@@ -16,4 +16,7 @@ urlpatterns = [
     path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
     
     path('update-user/', views.updateUser, name="update-user"),
+    
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
 ]
--- base/views.py
+++ base/views.py
@@ -65,7 +65,7 @@ def home(request):
                                 Q(description__icontains=q)
                                 )
     
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
     
@@ -171,3 +171,12 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
     
     return render(request, 'base/update-user.html', {'form': form})
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
