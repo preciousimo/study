794f8124d9a1bfb1c3dd4bf7bf5456782a975783
preciousimo <preciousimoniakemu@gmail.com>
2022-03-01 16:28:37 +0100

    Change DB to postgresql

--- studybud/settings.py
+++ studybud/settings.py
@@ -96,12 +96,14 @@ ALLOWED_HOSTS = ['*', '.***.com', '.herokuapp.com', 'localhost', '127.0.0.1']
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(BASE_DIR / 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = str(os.getenv('DATABASES'))
 
 
 # Password validation
