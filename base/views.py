45feefefcaeda0f5da9f16f47aa47a725bbe1018
preciousimo <preciousimoniakemu@gmail.com>
2022-03-02 13:22:08 +0100

    Fix user issue

--- base/views.py
+++ base/views.py
@@ -4,11 +4,13 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
 from .forms import RoomForm , UserForm
+
+User = get_user_model()
 
 def loginPage(request):
     
