6f18ee136ad931d967806d00ff4beb383d146bc7
preciousimo <preciousimoniakemu@gmail.com>
2022-03-02 14:09:07 +0100

    Clean up

--- base/models.py
+++ base/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-
 class User(AbstractUser):
+    username = models.CharField(max_length=30, unique=True)
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
 
     avatar = models.ImageField(null=True, default="avatar.svg")
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
+    USERNAME_FIELD = 'username'
+    REQUERID_FIELDS = ['email']
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
