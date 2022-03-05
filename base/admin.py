519c17904942a7ea7b63e55fed9767e9e57fdaed
Mary Solomon <johnsolo2002@gmail.com>
2022-03-05 19:22:13 +0100

    Implemented django-jazzmin theme successfully

--- base/admin.py
+++ base/admin.py
@@ -4,61 +4,59 @@ from django.contrib import admin
 
 from .models import User, Topic, Room, Message
 
-
 class UserAdmin(admin.ModelAdmin):
     
-    list_display = (
+    list_display = [
         'name',
         'username',
         'email',
         'bio'
-    )
+    ]
 
-    list_filter = (
+    list_filter = [
         'name',
         'username',
         'email',
-    )
+    ]
 
 class TopicAdmin(admin.ModelAdmin):
     
-    list_display = (
+    list_display = [
         'name'
-    )
+    ]
 
-    list_filter = (
+    list_filter = [
         'name'
-    )
+    ]
 
 class RoomAdmin(admin.ModelAdmin):
     
-    list_display = (
+    list_display = [
         'name',
         'topic',
         'description',
         'host',
-        'participants'
-    )
+    ]
 
-    list_filter = (
+    list_filter = [
         'name',
         'topic',
         'description'
-    )
+    ]
 
 class MessageAdmin(admin.ModelAdmin):
     
-    list_display = (
+    list_display = [
         'user',
         'room',
         'body'
-    )
+    ]
 
-    list_filter = (
+    list_filter = [
         'user'
-    )
+    ]
 
-admin.site.register(User)
-admin.site.register(Topic)
-admin.site.register(Room)
-admin.site.register(Message)
+admin.site.register(User, UserAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Message, MessageAdmin)
