6160608b9eb6307621bdc6817ee12f3258b9fa18
Mary Solomon <johnsolo2002@gmail.com>
2022-03-05 15:58:50 +0100

    Added python-decouple to requirements.txt

--- base/models.py
+++ base/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+
 class User(AbstractUser):
     username = models.CharField(max_length=30, unique=True)
     name = models.CharField(max_length=200, null=True)
