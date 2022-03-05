b5adba0db8a4f1cc8b70f5f57f39aa6417ba5ded
Mary Solomon <johnsolo2002@gmail.com>
2022-03-05 16:38:17 +0100

    Admin models created

--- base/admin.py
+++ base/admin.py
@@ -2,9 +2,48 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Room, Topic, Message, User
+from .models import User, Topic, Room, Message
 
+
+class UserAdmin(admin.ModelAdmin):
+    
+    list_display = (
+
+    )
+
+    list_filter = (
+
+    )
+
+class TopicAdmin(admin.ModelAdmin):
+    
+    list_display = (
+
+    )
+
+    list_filter = (
+
+    )
+class RoomAdmin(admin.ModelAdmin):
+    
+    list_display = (
+
+    )
+
+    list_filter = (
+
+    )
+
+class MessageAdmin(admin.ModelAdmin):
+    
+    list_display = (
+
+    )
+
+    list_filter = (
+
+    )
 admin.site.register(User)
-admin.site.register(Room)
 admin.site.register(Topic)
+admin.site.register(Room)
 admin.site.register(Message)
