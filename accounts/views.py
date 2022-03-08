8358662aa7ce553dfb3e790b5791cd1645de6c88
Mary Solomon <johnsolo2002@gmail.com>
2022-03-08 21:57:25 +0100

    Imported modules for user auth in accounts.views

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, Logout
+from django.contrib import messages
+
+
 # Create your views here.
 def registerPage(request):
     return HttpResponse('Registration Page... ')
