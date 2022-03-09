fad6394217bdb06da6fad0ce06a95e01341d1e6f
Mary Solomon <johnsolo2002@gmail.com>
2022-03-09 21:27:18 +0100

    Imported forms.py from base app

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .models import User
+from .forms import UserForm, MyUserCreationForm
 
 # Create your views here.
 def registerPage(request):
-    return render(request, 'register.html')
-
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+    else:
+        return render(request, 'register.html')
 def loginPage(request):
     return render(request, 'login.html')
 
