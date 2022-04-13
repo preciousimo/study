8a1380dfd41eac1b8398e92660fc6f71293e114e
preciousimo <preciousimoniakemu@gmail.com>
2022-04-13 17:22:38 +0100

    Comment url path for accoun

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Path to login
     path('login/', views.loginPage, name="login"),
+    
+    # path to logout
     path('logout/', views.logoutUser, name="logout"),
+
+    # path to registration
     path('register/', views.registerPage, name="register"),    
 ]
