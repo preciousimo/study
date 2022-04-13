c450cce3c7e8137aea7cf524b9e965b5b1c68daf
preciousimo <preciousimoniakemu@gmail.com>
2022-04-13 17:19:51 +0100

    Comment regitration page

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,19 +2,20 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from base.models import Room, User
 
-
+# Form details for user while creating
 class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['name', 'username', 'email', 'password1', 'password2']
 
+# Form details for application room
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
         exclude = ['host', 'participants']
 
-
+# Form details for user
 class UserForm(ModelForm):
     class Meta:
         model = User
